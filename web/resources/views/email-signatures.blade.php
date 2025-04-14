904e0e61d559876570640b45c1abe3b97dd7271a
Jerram Watters <jerramw@gmail.com>
2025-04-14 15:41:45 +1000

    Email signature v0.11

--- web/resources/views/email-signatures.blade.php
+++ web/resources/views/email-signatures.blade.php
@@ -36,53 +36,79 @@ function generateSignature($name, $email, $phone) {
     $s3BaseUrl = "https://aeyia-email-signatures.s3.ap-southeast-2.amazonaws.com";
 
     return '
-<div style="font-family: Arial, Helvetica, sans-serif; color: #212025; max-width: 600px;">
-    <table cellpadding="0" cellspacing="0" border="0" style="border-collapse: collapse;">
+<div style="font-family: Arial, Helvetica, sans-serif; color: #212025; width: 100%; max-width: 600px; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;">
+    <table cellpadding="0" cellspacing="0" border="0" style="border-collapse: collapse; width: 100%">
         <tr>
-            <!-- Name and Social Icons Column -->
-            <td style="vertical-align: top; min-width:150px;">
-                <div style="font-size: 17pt; color: #212025; font-weight: normal; padding-bottom: 10px;">' . htmlspecialchars($name) . '</div>
-                <div style="padding-top: 5px;">
-                    <a href="https://www.instagram.com/aeyia_/" style="text-decoration: none !important; display: inline-block; margin-right: 4px;">
-                        <img src="' . $s3BaseUrl . '/instagram.png" alt="Instagram" style="width: 21px; height: 21px; border: 0;">
+            <td style="vertical-align: top;">
+                <div style="font-size: large !important; color: #212025; font-weight: normal;">' . htmlspecialchars($name) . '<span style="display:none"></br>Aeyia</span></div>
+
+                <div style="padding-top: 15px;">
+                    <a href="https://www.instagram.com/aeyia_/" style="text-decoration: none !important; display: inline-block; margin-right: 3px;">
+                        <img src="' . $s3BaseUrl . '/instagram.png" alt="Instagram" style="width: 1.3rem !important; border: 0;">
                     </a>
-                    <a href="http://www.facebook.com/aeyia.au" style="text-decoration: none !important; display: inline-block; margin-right: 4px;">
-                        <img src="' . $s3BaseUrl . '/facebook.png" alt="Facebook" style="width: 21px; height: 21px; border: 0;">
+                    <a href="http://www.facebook.com/aeyia.au" style="text-decoration: none !important; display: inline-block; margin-right: 3px;">
+                        <img src="' . $s3BaseUrl . '/facebook.png" alt="Facebook" style="width: 1.3rem !important; border: 0;">
                     </a>
-                    <a href="http://www.tiktok.com/@aeyia__" style="text-decoration: none !important; display: inline-block; margin-right: 4px;">
-                        <img src="' . $s3BaseUrl . '/tiktok.png" alt="TikTok" style="width: 21px; height: 21px; border: 0;">
+                    <a href="http://www.tiktok.com/@aeyia__" style="text-decoration: none !important; display: inline-block; margin-right: 3px;">
+                        <img src="' . $s3BaseUrl . '/tiktok.png" alt="TikTok" style="width: 1.3rem !important; border: 0;">
                     </a>
                     <a href="https://www.youtube.com/@AEYIAWellness" style="text-decoration: none !important; display: inline-block;">
-                        <img src="' . $s3BaseUrl . '/youtube.png" alt="YouTube" style="width: 21px; height: 21px; border: 0;">
+                        <img src="' . $s3BaseUrl . '/youtube.png" alt="YouTube" style="width: 1.3rem !important; border: 0;">
                     </a>
                 </div>
             </td>
-        </tr>
-        <tr>
-            <!-- Contact Information Column -->
-            <td style="padding-top: 10px; vertical-align: top;">
-                <div style="margin-bottom: 5px;">
-                    <img src="' . $s3BaseUrl . '/phone.png" alt="" style="width: 12px; height: 12px; vertical-align: middle;">
-                    <span style="font-size: 8pt; color: #212025; vertical-align: middle;">&nbsp;&nbsp;' . htmlspecialchars($phone) . '</span>
-                </div>
-                <div style="margin-bottom: 5px;">
-                    <img src="' . $s3BaseUrl . '/mail.png" alt="" style="width: 12px; height: 12px; vertical-align: middle;">&nbsp;&nbsp;
-                    <a href="mailto:' . htmlspecialchars($email) . '" style="font-size: 8pt; color: #212025; text-decoration: none !important; vertical-align: middle;">' . htmlspecialchars($email) . '</a>
-                </div>
-                <div style="margin-bottom: 5px;">
-                    <img src="' . $s3BaseUrl . '/web.png" alt="" style="width: 12px; height: 12px; vertical-align: middle;">&nbsp;&nbsp;
-                    <a href="http://www.aeyia.com" style="font-size: 8pt; color: #212025; text-decoration: none !important; vertical-align: middle;">www.aeyia.com</a>
-                </div>
-                <div>
-                    <img src="' . $s3BaseUrl . '/map.png" alt="" style="width: 12px; height: 12px; vertical-align: middle;">
-                    <span style="font-size: 8pt; color: #212025; vertical-align: middle;">&nbsp;&nbsp;6/7 Grevillea Street, Byron Bay</span>
-                </div>
+
+            <td style="padding: 0 15px; vertical-align: top;">
+                <div style="width: 1px; background-color: #666666; height: 100%; min-height: 100px;"></div>
+            </td>
+
+            <td style="vertical-align: top;">
+                <table cellpadding="0" cellspacing="0" border="0" style="vertical-align: -webkit-baseline-middle;">
+                    <tbody>
+                        <tr style="vertical-align: middle; height: 25px;">
+                            <td width="30" style="vertical-align: middle;">
+                                <img src="' . $s3BaseUrl . '/phone.png" alt="" style="width: 1.1rem !important; display: block;">
+                            </td>
+                            <td style="padding: 0px;">
+                                <a href="tel:' . htmlspecialchars($phone) . '" style="font-size: small !important; color: #212025; text-decoration-line: none;">' . htmlspecialchars($phone) . '</a></span>
+                            </td>
+                        </tr>
+                        <tr style="vertical-align: middle; height: 25px;">
+                            <td width="30" style="vertical-align: middle;">
+                                <img src="' . $s3BaseUrl . '/mail.png" alt="" style="width: 1.1rem !important; display: block;">
+                            </td>
+                            <td style="padding: 0px;">
+                                <a href="mailto:' . htmlspecialchars($email) . '" style="font-size: small !important; color: #212025; text-decoration: none !important;">' . htmlspecialchars($email) . '</a>
+                            </td>
+                        </tr>
+                        <tr style="vertical-align: middle; height: 25px;">
+                            <td width="30" style="vertical-align: middle;">
+                                <img src="' . $s3BaseUrl . '/web.png" alt="" style="width: 1.1rem !important; display: block;">
+                            </td>
+                            <td style="padding: 0px;">
+                                <a href="http://www.aeyia.com" style="font-size: small !important; color: #212025; text-decoration: none !important;">www.aeyia.com</a>
+                            </td>
+                        </tr>
+                        <tr style="vertical-align: middle; height: 25px;">
+                            <td width="30" style="vertical-align: middle;">
+                                <a href="https://maps.google.com/?q=6/7 Grevillea Street, Byron Bay, NSW 2481" style="text-decoration: none !important;" target="_blank">
+                                    <img src="' . $s3BaseUrl . '/map.png" alt="" style="width: 1.1rem !important; display: block;">
+                                </a>
+                            </td>
+                            <td style="padding: 0px;">
+                                <a href="https://maps.google.com/?q=6/7 Grevillea Street, Byron Bay, NSW 2481" style="font-size: small !important; color: #212025; text-decoration: none !important;" target="_blank">
+                                    6/7 Grevillea Street, Byron Bay
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </td>
         </tr>
     </table>
 
     <!-- Logo row -->
-    <table cellpadding="0" cellspacing="0" border="0" style=" margin-top: 5px;" border=1 bg-color="red">
+    <table cellpadding="0" cellspacing="0" border="0" style="margin-top: 15px;" border=1 bg-color="red">
         <tr>
             <td>
                 <img src="' . $s3BaseUrl . '/aeyia-logo.png" alt="AEYIA" style="max-width: 250px; height: auto; border: 0;">
@@ -94,9 +120,9 @@ function generateSignature($name, $email, $phone) {
     <table cellpadding="0" cellspacing="0" border="0" style="border-collapse: collapse; margin-top: 15px;">
         <tr>
             <td>
-                <p style="font-size: 6pt; color: #808080; margin: 0; padding-bottom: 5px;">AEYIA respectfully acknowledges the Traditional Owners of the land on which we live and work. We pay our respects to Elders past, present, and future and recognise their ongoing connection to land, waters, and culture. Their wisdom and resilience continue to inspire and guide us.</p>
-                <p style="font-size: 6pt; color: #808080; margin: 0; padding-bottom: 5px;">CONFIDENTIALITY NOTICE: The contents of this email are confidential to the ordinary user of the email address to which it is addressed and may also be privileged. If you are not the addressee of this email, you may not copy, forward, disclose or otherwise use it or any part of it in any form whatsoever. If you have received this email by error, please email the sender by replying to this message.</p>
-                <p style="font-size: 6pt; color: #808080; margin: 0;">Please consider the environment before printing this e-mail.</p>
+                <p style="font-size: 10px !important; color: #808080; margin: 0; padding-bottom: 5px;">AEYIA respectfully acknowledges the Traditional Owners of the land on which we live and work. We pay our respects to Elders past, present, and future and recognise their ongoing connection to land, waters, and culture. Their wisdom and resilience continue to inspire and guide us.</p>
+                <p style="font-size: 10px !important; color: #808080; margin: 0; padding-bottom: 5px;">CONFIDENTIALITY NOTICE: The contents of this email are confidential to the ordinary user of the email address to which it is addressed and may also be privileged. If you are not the addressee of this email, you may not copy, forward, disclose or otherwise use it or any part of it in any form whatsoever. If you have received this email by error, please email the sender by replying to this message.</p>
+                <p style="font-size: 10px !important; color: #808080; margin: 0;">Please consider the environment before printing this e-mail. <br/>v0.11</p>
             </td>
         </tr>
     </table>
@@ -108,7 +134,7 @@ function generateSignature($name, $email, $phone) {
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <title>AEYIA Email Signature Generator</title>
     <style>
         body {
@@ -116,13 +142,12 @@ function generateSignature($name, $email, $phone) {
             max-width: 1000px;
             margin: 0 auto;
             padding: 20px;
-            background-color: #f5f5f5;
+            background-color: #fff;
         }
         .container {
-            background-color: white;
-            border-radius: 8px;
-            padding: 30px;
-            box-shadow: 0 2px 10px rgba(0,0,0,0.1);
+            padding: 0;
+            width: 100%;
+            max-width: 100%;
         }
         h1 {
             color: #474747;
@@ -135,6 +160,7 @@ function generateSignature($name, $email, $phone) {
             gap: 10px;
             margin-bottom: 30px;
             justify-content: center;
+            padding: 0 20px;
         }
         button, .btn {
             background-color: #474747;
@@ -151,17 +177,18 @@ function generateSignature($name, $email, $phone) {
             background-color: #666;
         }
         #signature-preview {
-            margin-top: 20px;
-            border: 1px solid #ddd;
-            padding: 20px;
-            border-radius: 5px;
+            border-top:1px solid #222;
+            margin:0;
+            padding: 20px 0;
+            width: 100%;
+            max-width: 100%;
         }
         .instructions {
             margin-top: 20px;
             padding: 15px;
             background-color: #f0f8ff;
             border-radius: 5px;
-            border-left: 4px solid #4682b4;
+            border: 4px solid #4682b4;
         }
         .copy-btn {
             display: block;
@@ -169,6 +196,9 @@ function generateSignature($name, $email, $phone) {
             background-color: #4682b4;
             padding: 12px 25px;
             font-size: 16px;
+        }
+        #copy-section {
+            padding: 0 20px;
         }
         .hidden {
             display: none;
@@ -193,16 +223,16 @@ function generateSignature($name, $email, $phone) {
 </head>
 <body>
     <div class="container">
-        <h1>AEYIA Email Signature Generator</h1>
+            <h1 style="padding: 20px 20px 0 20px; margin-top: 0;">AEYIA Email Signature Generator</h1>
 
-        <form method="get" class="team-buttons">
-            @foreach ($team_members as $index => $member)
-                @php
-                    $firstName = explode(' ', $member['name'])[0];
-                @endphp
-                <button type="submit" name="member_id" value="{{ $index }}" class="btn">{{ $firstName }}</button>
-            @endforeach
-        </form>
+            <form method="get" class="team-buttons">
+                @foreach ($team_members as $index => $member)
+                    @php
+                        $firstName = explode(' ', $member['name'])[0];
+                    @endphp
+                    <button type="submit" name="member_id" value="{{ $index }}" class="btn">{{ $firstName }}</button>
+                @endforeach
+            </form>
 
         @if ($selectedMember)
             <div id="copy-section">
@@ -212,11 +242,11 @@ function generateSignature($name, $email, $phone) {
                 <button id="copy-btn" class="copy-btn" onclick="copyToClipboard()">Copy Signature to Clipboard</button>
             </div>
 
-            <div id="signature-preview">
+            <div id="signature-preview" style="margin-top: 20px; margin-bottom: 20px;">
                 {!! $signatureHtml !!}
             </div>
 
-            <div id="alternative-options" class="hidden">
+            <div id="alternative-options" class="hidden" style="padding: 0 20px;">
                 <div class="instructions">
                     <h3>Alternative Options:</h3>
                     <p>If the copy button didn't work, you can try these options:</p>
@@ -232,7 +262,7 @@ function generateSignature($name, $email, $phone) {
                 </div>
             </div>
 
-            <div class="instructions" id="instructions">
+            <div class="instructions" id="instructions" style="margin: 20px;">
                 <h3>How to add this signature to Gmail:</h3>
                 <ol>
                     <li>Click the "Copy Signature to Clipboard" button above</li>
