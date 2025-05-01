9f41af985cdebbc4ecc7b97b128dab7ca794b0a7
Jerram Watters <jerramw@gmail.com>
2025-05-01 13:04:56 +1000

    Auth card transparency, font size and colour

--- web/resources/js/Pages/Auth/ForgotPassword.vue
+++ web/resources/js/Pages/Auth/ForgotPassword.vue
@@ -28,11 +28,11 @@ const submit = () => {
             <AuthenticationCardLogo />
         </template>
 
-        <div class="mb-4 text-sm text-gray-600">
+        <div class="mb-4 text-base text-white">
             Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
         </div>
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+        <div v-if="status" class="mb-4 font-medium text-base text-green-600">
             {{ status }}
         </div>
 
