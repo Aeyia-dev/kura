865aa9ab58b294bed12f3b46325d621a502c2fef
Jerram Watters <jerramw@gmail.com>
2025-05-20 17:50:49 +1000

    Add video for mac mail html sig set up

--- web/resources/views/email-signatures.blade.php
+++ web/resources/views/email-signatures.blade.php
@@ -309,6 +309,7 @@ function generateSignature($name, $email, $phone) {
             <div class="instructions" id="instructions" style="margin: 20px;">
                 <h3>How to add this signature to Mac Mail:</h3>
                 <ol>
+                    <li><a href="https://youtu.be/Y7kho6OT1qk?si=6ZtJwvMRWn_xYipO&t=36"target="_blank">Video walk through</a></li>
                     <li>Open Mac Mail, then Mail > Settings > Signatures</li>
                     <li>Click + icon to create signature in Mac Mail, name it AEYIA</li>
                     <li>Add some placeholder text like "New Aeyia placeholder signature"</li>
